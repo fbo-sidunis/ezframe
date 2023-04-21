1e2bf453ec8665b80a82477421f372eb20cce455
Mathieu NADEAU <mnadeau@groupefbo.com>
2023-04-21 15:58:15 +0200

    twigjs

--- libs/twigjs/test/compiler/templates/sub/sub.twig.js
+++
@@ -1,4 +0,0 @@
-module.declare([{twig: 'vendor/twig'}], (require, exports, module) => {
-    const {twig} = require('twig');
-    exports.template = twig({id: 'templates/sub/sub.twig', data: [{type: 'raw', value: 'I\'m a '}, {type: 'output', stack: [{type: 'Twig.expression.type.variable', value: 'type', match: ['type']}]}, {type: 'raw', value: '\n'}], precompiled: true});
-});

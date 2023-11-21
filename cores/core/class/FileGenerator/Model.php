befa8048e08cf61882fea43a0072ab6d5062961b
Mathieu NADEAU <mnadeau@groupefbo.com>
2023-11-21 11:10:07 +0100

    Corrections

--- cores/core/class/FileGenerator/Model.php
+++ cores/core/class/FileGenerator/Model.php
@@ -31,7 +31,7 @@ class Model extends FileGenerator
     $className = self::snakeCaseToPascalCase($tableName);
     $fileFullPath = self::PATH . $className . '.php';
     if (file_exists($fileFullPath)) {
-      throw new Exception("Le fichier $fileFullPath existe déjà, veuillez le supprimer pour le regénérer");
+      unlink($fileFullPath);
     }
     $primaryKeys = $table->getPrimaryKeys();
     if (count($primaryKeys) < 1) {
@@ -53,22 +53,38 @@ class Model extends FileGenerator
     $content[] = "{";
     $content[] = "\tpublic static \$tbl = \"$tableName\";";
     $content[] = "\tpublic static \$pkey = \"{$primaryKey->getName()}\";";
-    $content[] = "";
+    $content[] = "\t";
+    self::addGetByFiltersMethod($table, $content);
+    $content[] = "\t";
+    self::addGetColumns($table, $content);
+    $content[] = "\t";
+    self::addGetJoinTablesMethod($table, $content);
+    $content[] = "\t";
+    self::addGetGroupByMethod($table, $content);
+    $content[] = "\t";
+    self::addGetOrderColumnsAuthorizedMethod($table, $content);
+    $content[] = "\t";
+    self::addAddFiltersMethod($table, $content);
+    $content[] = "\t";
+    self::addGetSpecialQueryMethod($table, $content);
+    $content[] = "}";
+    self::putInFile($fileFullPath, $content);
+  }
+
+  protected static function addGetByFiltersMethod(
+    Table $table,
+    array &$content
+  ) {
     $content[] = "\tpublic static function getByFilters(";
     $content[] = "\t\tarray \$queryDatas = [],";
     $content[] = "\t\tarray \$filters = [],";
     $content[] = "\t\t?string \$specialQuery = null";
     $content[] = "\t) {";
-    //Début du code de la méthode
-    //Début Init
     $content[] = "\t\t\$query = new SQLBuilder([";
-    $content[] = "\t\t\t\"colonnes\" => [";
-    $content[] = "\t\t\t\tself::al(\"*\"),";
-    $content[] = "\t\t\t],";
-    $content[] = "\t\t\t\"table\" => self::\$tbl,";
-    $content[] = "\t\t\t\"groupBy\" => [";
-    $content[] = "\t\t\t\tself::al(self::\$pkey),";
-    $content[] = "\t\t\t],";
+    $content[] = "\t\t\t\"colonnes\" => self::getColumns(),";
+    $content[] = "\t\t\t\"table\" => self::tbl(),";
+    $content[] = "\t\t\t\"joinTables\" => self::getJoinTables(),";
+    $content[] = "\t\t\t\"groupBy\" => self::getGroupBy(),";
     $content[] = "\t\t\t\"limit\" => \$queryDatas[\"length\"] ?? null,";
     $content[] = "\t\t\t\"offset\" => \$queryDatas[\"start\"] ?? null,";
     $content[] = "\t\t]);";
@@ -77,11 +93,7 @@ class Model extends FileGenerator
     //Début de l'order by'
     $content[] = "\t\t\$orderCol = \$queryDatas[\"orderCol\"] ?? null;";
     $content[] = "\t\t\$direction = \$queryDatas[\"direction\"] ?? null;";
-    $content[] = "\t\t\$orderColumnsAuthorized = [";
-    foreach ($table->getColumns() as $column) {
-      $content[] = "\t\t\t\"{$column->getName()}\" => self::al(\"{$column->getName()}\"),";
-    }
-    $content[] = "\t\t];";
+    $content[] = "\t\t\$orderColumnsAuthorized = self::getOrderColumnsAuthorized();";
     $content[] = "\t\tif (in_array(\$orderCol, array_keys(\$orderColumnsAuthorized)) && in_array(\$direction, [\"ASC\", \"DESC\"])) {";
     $content[] = "\t\t\t\$query->addOrderBy(\$orderColumnsAuthorized[\$orderCol] . \" \" . \$direction);";
     $content[] = "\t\t}";
@@ -105,6 +117,9 @@ class Model extends FileGenerator
       $content[] = "\t\t}";
       $content[] = "\t\t";
     }
+    $content[] = "\t\t";
+    $content[] = "\t\tself::addFilters(\$query, \$filters);";
+    $content[] = "\t\t";
     //Fin des filtres
     //Début des SpecialQuery (case count, id et ids)
     $content[] = "\t\tif (\$specialQuery !== null) {";
@@ -128,7 +143,7 @@ class Model extends FileGenerator
     $content[] = "\t\t\t\t\treturn self::db_all(\$query->getSql(), \$query->getValues(), PDO::FETCH_COLUMN);";
     $content[] = "\t\t\t\t\tbreak;";
     $content[] = "\t\t\t\tdefault:";
-    $content[] = "\t\t\t\t\tthrow new Exception(\"Special query \$specialQuery not implemented\");";
+    $content[] = "\t\t\t\t\treturn self::getSpecialQuery(\$query, \$queryDatas, \$filters, \$specialQuery);";
     $content[] = "\t\t\t\t\tbreak;";
     $content[] = "\t\t\t}";
     $content[] = "\t\t}";
@@ -137,7 +152,70 @@ class Model extends FileGenerator
     $content[] = "\t\treturn self::db_all(\$query->getSql(), \$query->getValues());";
     //Fin du code de la méthode
     $content[] = "\t}";
-    $content[] = "}";
-    self::putInFile($fileFullPath, $content);
+  }
+
+  protected static function addGetOrderColumnsAuthorizedMethod(
+    Table $table,
+    array &$content
+  ) {
+    $content[] = "\tprotected static function getOrderColumnsAuthorized() {";
+    $content[] = "\t\treturn [";
+    foreach ($table->getColumns() as $column) {
+      $content[] = "\t\t\t\"{$column->getName()}\" => self::al(\"{$column->getName()}\"),";
+    }
+    $content[] = "\t\t];";
+    $content[] = "\t}";
+  }
+
+  protected static function addGetJoinTablesMethod(
+    Table $table,
+    array &$content
+  ) {
+    $content[] = "\tprotected static function getJoinTables() {";
+    $content[] = "\t\t//Pour customiser les jointures, il faut surcharger cette méthode dans le modèle";
+    $content[] = "\t\treturn [];";
+    $content[] = "\t}";
+  }
+
+  protected static function addGetColumns(
+    Table $table,
+    array &$content
+  ) {
+    $content[] = "\tprotected static function getColumns() {";
+    $content[] = "\t\treturn [";
+    $content[] = "\t\t\tself::al(\"*\"),";
+    $content[] = "\t\t];";
+    $content[] = "\t}";
+  }
+
+  protected static function addAddFiltersMethod(
+    Table $table,
+    array &$content
+  ) {
+    $content[] = "\tprotected static function addFilters(SQLBuilder \$query, array \$filters) {";
+    $content[] = "\t\t//Pour customiser les filtres, il faut surcharger cette méthode dans le modèle";
+    $content[] = "\t}";
+  }
+
+  protected static function addGetSpecialQueryMethod(
+    Table $table,
+    array &$content
+  ) {
+    $content[] = "\tprotected static function getSpecialQuery(SQLBuilder \$query, array \$queryDatas, array \$filters, string \$specialQuery) {";
+    $content[] = "\t\t//Pour customiser les requêtes spéciales, il faut surcharger cette méthode dans le modèle";
+    $content[] = "\t\tthrow new Exception(\"La requête spéciale \$specialQuery n'est pas supportée\");";
+    $content[] = "\t}";
+  }
+
+  protected static function addGetGroupByMethod(
+    Table $table,
+    array &$content
+  ) {
+    $content[] = "\tprotected static function getGroupBy() {";
+    $content[] = "\t\t//Pour customiser les group by, il faut surcharger cette méthode dans le modèle";
+    $content[] = "\t\treturn [";
+    $content[] = "\t\t\tself::al(self::\$pkey),";
+    $content[] = "\t\t];";
+    $content[] = "\t}";
   }
 }
